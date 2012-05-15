7607fda9680a0e64141a500204cbc27205436096
Jam The Best <jam@Jam-Ubuntu.(none)>
2012-05-15 11:07:49 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '3efb02b50ebd7fa21fec71741acf237fd71d58c6b2e933199b826fd20b03f0bc9b3e4de99b1a96d6272f9eea92843a542f18d521609e8fd6c4cf7351ed990b1a'
